109b12702af49fedacd817011bdfeea822782d6d
EnejBrus <enej.brus@gmail.com>
2024-07-05 11:13:13 +0200

    streznik

--- generator1/src/main.rs
+++ generator1/src/main.rs
@@ -97,6 +97,119 @@ fn get_sequence(Sequence) -> SequenceInfo {
     }
 }
 
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
 
+async fn collect_body(req: Request<Incoming>) -> Result<String, hyper::Error> {
+    let max = req.body().size_hint().upper().unwrap_or(u64::MAX);
+    if max > 1024 * 64 {
+        panic!("Body too big");
+    }
+
+    let whole_body = req.collect().await?.to_bytes();
+    let whole_body = std::str::from_utf8(&whole_body).unwrap().to_string();
+    return Ok(whole_body);
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+async fn send_post(url: String, body: String) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let res = client.post(url).body(body).send().await?.text().await?;
+    return Ok(res);
+}
+
+async fn send_get(url: String) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let res = client.get(url).send().await?.text().await?;
+    return Ok(res);
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr: SocketAddr = ([0, 0, 0, 0], PORT).into();
+
+    let b = send_get("http://0.0.0.0:7878/project".to_string()).await?;
+    println!("HERE {}", b);
+
+    let b = send_post(
+        "http://0.0.0.0:7878/project".to_string(),
+        serde_json::to_string(&get_project()).unwrap(),
+    )
+    .await?;
+    println!("HERE {}", b);
+
+    let b = send_get("http://0.0.0.0:7878".to_string()).await?;
+    println!("HERE {}", b);
+
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+
+    let create_404 = || {
+        let mut not_found = Response::new(empty());
+        *not_found.status_mut() = StatusCode::NOT_FOUND;
+        Ok(not_found)
+    };
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            let service = service_fn(move |req| {
+                async move {
+                    match (req.method(), req.uri().path()) {
+                        (&Method::GET, "/ping") => Ok::<_, Error>(Response::new(full(
+                            serde_json::to_string(&get_project()).unwrap(),
+                        ))),
+                        (&Method::GET, "/sequence") => {
+                            //
+                            let sequences = sequences();
+                            Ok(Response::new(full(
+                                serde_json::to_string(&sequences).unwrap(),
+                            )))
+                        }
+                        (&Method::POST, r) => {
+                            let seqs = sequences();
+                            let sequences = seqs
+                                .iter()
+                                .find(|&x| ("/sequence/".to_string() + &x.name) == r);
+                            match sequences {
+                                None => create_404(),
+                                Some(s) if *s.name == "Arithmetic".to_string() => {
+                                    let body = collect_body(req).await?;
+                                    let request: SequenceRequest =
+                                        serde_json::from_str(&body).unwrap();
+                                    let range = request.range;
+                                    let seq = Arithmetic::new(
+                                        request.parameters[0],
+                                        request.parameters[1],
+                                    );
+                                    Ok(Response::new(full(
+                                        serde_json::to_string(&seq.range(range)).unwrap(),
+                                    )))
+                                }
+                                _ => panic!("Not implemented"),
+                            }
+                        }
+
+                        _ => create_404(),
+                    }
+                }
+            });
+
+            if let Err(err) = http1::Builder::new().serve_connection(io, service).await {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
 
 fn main() {}
