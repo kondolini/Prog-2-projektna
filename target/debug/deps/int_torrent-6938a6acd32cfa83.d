995cc1c05e2e9e20158bf8dd323828a819c13b3c
Anze Gartner <ag67236@student.uni-lj.si>
2024-10-25 15:59:06 +0200

    pika na i

--- target/debug/deps/int_torrent-6938a6acd32cfa83.d
+++
@@ -1,6 +0,0 @@
-C:\Users\ag67236\OneDrive\prog2-projekt_KLON\Prog-2-projektna\target\debug\deps\libint_torrent-6938a6acd32cfa83.rmeta: src/main.rs src\sequence.rs
-
-C:\Users\ag67236\OneDrive\prog2-projekt_KLON\Prog-2-projektna\target\debug\deps\int_torrent-6938a6acd32cfa83.d: src/main.rs src\sequence.rs
-
-src/main.rs:
-src\sequence.rs:
